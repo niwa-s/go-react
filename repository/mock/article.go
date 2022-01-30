84c9f2caa40c83929998f099dd5295b75ff7aba7
niwa-s <souta.niwa.prog@gmail.com>
2022-01-30 09:01:02 +0900

    a

--- repository/mock/article.go
+++ repository/mock/article.go
@@ -12,3 +12,29 @@ func (repo Repository) GetAllArticles(ctx context.Context) ([]repository.Article
 
 	return mockArticles, nil
 }
+
+func (repo Repository) GetAllArticle(ctx context.Context, id int64) (repository.Article, error) {
+	for _, a := range mockArticles {
+		if a.ID == id {
+			return a, nil
+		}
+	}
+	return repository.Article{}, repository.ErrArticleNotFound
+}
+
+func (repo Repository) SaveArticle(ctx context.Context, title, content, author string, createdAt time.Time) (repository.Article, error) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	var article repository.Article
+	article.ID = articleIDCounter
+	article.Title = title
+	article.Content = content
+	article.Author = author
+	article.CreatedAt = createdAt
+
+	mockArticles = append(mockArticles, article)
+	articleIDCounter++
+
+	return article, nil
+}
